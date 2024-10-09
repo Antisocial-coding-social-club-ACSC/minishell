9e7f07f2d4ce084d51379d0ecc250a51da042c8e
Alex Collet Martínez <aleexcolleet@AlexCollet-MacBook-Pro.local>
2024-10-09 19:31:07 +0200

    0910 - Added get_next_line, libt and organized folders

---
+++ includes/minishell.h
@@ -0,0 +1,12 @@
+#ifndef MINISHELL_H
+#define MINISHELL_H
+
+# include "../libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <fcntl.h>
+
+
+#endif
